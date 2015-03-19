d66333ac3fd7286eabf0b6424d2a2c31af46b31e
Jan Berktold <janberktold@aol.com>
2015-03-19 15:37:49 +0100

    add example for events

---
+++ examples/events/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/JanBerktold/sse"
+	"net/http"
+	"time"
+)
+
+func HandleSSE(w http.ResponseWriter, r *http.Request) {
+	conn, err := sse.Upgrade(w, r)
+
+	if err != nil {
+		// log error to console
+		fmt.Printf("Error occured: %q", err.Error())
+	}
+
+	for {
+		// Trigger event "time" with current time
+		conn.WriteStringEvent("time", time.Now().Format("Mon Jan 2 15:04:05 MST 2006"))
+		conn.WriteStringEvent("feed", "User XY did Z")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+
+	// handle server-sent events request
+	http.HandleFunc("/event", HandleSSE)
+
+	// serve HTML page
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "main.html")
+	})
+
+	http.ListenAndServe(":80", nil)
+}
